2efa5f795f935e9ffb807a02761894538ee8606d
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-03-10 00:44:45 +0700

    1 task solved; 00062-medium-type-lookup;

--- solved/medium/00062-medium-type-lookup.ts
+++ solved/medium/00062-medium-type-lookup.ts
@@ -29,7 +29,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type LookUp<U, T> = any
+type LookUp<U extends Animal, T extends Animal['type']> = {
+  [UKey in keyof U]: U[UKey] extends T ? U : never
+}['type']
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
