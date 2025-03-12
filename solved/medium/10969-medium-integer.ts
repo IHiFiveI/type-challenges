3b547f96281e3233296795659a00605f8f54b992
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-03-13 00:23:30 +0700

    1 task solved; 10969-medium-integer;

--- solved/medium/10969-medium-integer.ts
+++ solved/medium/10969-medium-integer.ts
@@ -12,7 +12,11 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Integer<T> = any
+type Integer<T extends number> = `${T}` extends `${string}.${string}`
+  ? never
+  : number extends T
+    ? never
+    : T
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
